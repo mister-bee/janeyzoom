6e2afc69d03bf5ba0ab40bd68d3ae157d48649f9
mister-bee <thebeech@yahoo.com>
2023-04-23 16:49:55 -0700

    heroku init

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ io.on('connection', socket => {
   })
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
